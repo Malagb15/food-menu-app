9fa8a441efc88fefc3bf89f2f962cda6d823d24e
Malagb15 <Malagb15@gmail.com>
2025-06-26 14:39:23 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+import img1 from "./img/1.jpeg";
+import img2 from "./img/2.jpeg";
+import img3 from "./img/3.jpeg";
+import img4 from "./img/4.jpeg";
+import img5 from "./img/5.jpeg";
+const menu = [
+  {
+    id: 1,
+    title: "Idli Sambar",
+    category: "Breakfast",
+    price: "₹40",
+    img: img1,
+    desc: "Soft idlis served with hot sambar.",
+  },
+  {
+    id: 2,
+    title: "Masala Dosa",
+    category: "Breakfast",
+    price: "₹50",
+    img: img2,
+    desc: "Crispy dosa stuffed with potato masala.",
+  },
+  {
+    id: 3,
+    title: "Veg Thali",
+    category: "Lunch",
+    price: "₹120",
+    img: img3,
+    desc: "Full veg meal with roti, rice, and curry.",
+  },
+  {
+    id: 4,
+    title: "Paneer Wrap",
+    category: "Snacks",
+    price: "₹60",
+    img: img4,
+    desc: "Wrap filled with spicy paneer and veggies.",
+  },
+  {
+    id: 5,
+    title: "Chole Bhature",
+    category: "Lunch",
+    price: "₹90",
+    img: img5,
+    desc: "Fluffy bhature with spicy chole.",
+  },
+];
+
+export default menu;
