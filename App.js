7124123c4184e3eeeb9bfdb9cba91063ce363a6f
Malagb15 <Malagb15@gmail.com>
2025-06-26 14:38:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+import items from "./data";
+import "./App.css";
+
+const allCategories = ["All", ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "All") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Food Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
